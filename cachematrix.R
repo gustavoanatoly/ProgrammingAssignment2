bfe8223a851b27d7a28bad518e24179386066333
Gustavo Anatoly <gustavoanatoly@gmail.com>
2015-12-23 15:34:53 -0300

    First implementation

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,50 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    solveMatrix <- NULL
+    
+    ## Set val to x and solveMatrix to NULL in envioronment.
+    set <- function(val) {
+        x <<- val
+        solveMatrix <<- NULL
+    }
+    
+    ## Return solveMatrix
+    get <- function() x
+    
+    ## Set solve to solveMatrix in envioronment.
+    setSolve <- function(solve) {
+        solveMatrix <<- solve
+    } 
+    
+    ## Get solveMatrix cached from envioronment.
+    getSolve <- function() solveMatrix
+    
+    ## Expose getters and setters
+    list(set = set, get = get, setSolve = setSolve, getSolve = getSolve)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Get solved cached matrix
+    inverseMatrix <- x$getSolve()
+
+    ## Check if inverseMatrix is different of null and return it.
+    if (!is.null(inverseMatrix)) {
+        message("Inverser matrix founded in cache")
+        return (inverseMatrix)
+    }
+
+    ## Get instance from environment.
+    data <- x$get()
+    
+    ## Calculate inverse of matrix
+    inverseMatrix <- data %*% solve(data)
+
+    ## Caching solve result
+    x$setSolve(inverseMatrix)
+
+    return(inverseMatrix)
 }
